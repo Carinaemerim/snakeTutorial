794d4fb0241b0623464f89c24bc07ce2f0170b87
carinaemerim2 <carinaemerim2@gmail.com>
2018-12-11 14:48:16 -0200

    inicio snake tutorial

---
+++ snake.py
@@ -0,0 +1,35 @@
+import pygame
+import pygame as pygame
+import random
+from pygame.locals import *
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Snake')
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((255, 255, 255))
+snake = [(200, 200), (210, 200), (220, 200)]
+
+apple_pos = (random.randint(0, 590), random.randint(0, 590))
+apple = pygame.Surface((10, 10))
+apple.fill((255, 0, 0))
+
+my_direction = LEFT
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+
+    screen.fill((0, 0, 0))
+    screen.blit(apple, apple_pos)
+    for pos in snake:
+        screen.blit(snake_skin, pos)
+
+    pygame.display.update()
